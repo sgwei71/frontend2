a6724936d8f686a3863f7db3617d880b6676a8cc
sunggyu <sunggyu@ibk.co.kr>
2025-04-01 01:19:27 +0900

    TrafficWeather 변경

--- src/app/components/TrafficWeather.jsx
+++ src/app/components/TrafficWeather.jsx
@@ -15,45 +15,41 @@ const TrafficWeather = ({ trafficInfo, weather }) => {
             <div className="flex justify-center items-center w-full h-full space-x-6 px-16 py-8">
                 {/* 교통상황 */}
                 <div className="flex flex-col justify-between bg-blue-common text-white font-gothic-bold shadow-lg rounded-lg w-[45%] h-full">
-                    <h2 className="text-center text-2xl text-shadow-md p-4">교통상황</h2>
+                    <h2 className="text-center text-2xl text-shadow-md p-4">진행사항</h2>
                     <div className="w-full p-4 bg-[#1e4da7] rounded-b-lg text-xl font-gothic">
                         <div className="flex justify-between items-center border-b border-black py-2">
-                            <span>성규 - 금곡교차</span>
-                            <span
-                                className={`w-4 h-4 rounded-full ${trafficColor[trafficInfo[0]]}`}
-                            ></span>
+                            <span>사전작업</span>
                         </div>
                         <div className="flex justify-between items-center border-b border-black py-2">
-                            <span>도시고속도로</span>
-                            <span
-                                className={`w-4 h-4 rounded-full ${trafficColor[trafficInfo[1]]}`}
-                            ></span>
+                            <span>서비스 차단</span>
                         </div>
                         <div className="flex justify-between items-center border-b border-black py-2">
-                            <span>송파IC - 상일IC</span>
-                            <span
-                                className={`w-4 h-4 rounded-full ${trafficColor[trafficInfo[2]]}`}
-                            ></span>
+                            <span>데이터복제</span>
                         </div>
                         <div className="flex justify-between items-center py-2">
-                            <span>상일IC - 하남센터</span>
-                            <span
-                                className={`w-4 h-4 rounded-full ${trafficColor[trafficInfo[3]]}`}
-                            ></span>
+                            <span>서버 시작</span>
                         </div>
                     </div>
                 </div>
 
                 {/* 날씨 */}
                 <div className="flex flex-col bg-blue-500 text-white font-gothic-bold shadow-lg rounded-lg w-[45%] h-full">
-                    <h2 className="text-center text-2xl p-4 text-shadow-md">날 씨</h2>
+                    <h2 className="text-center text-2xl p-4 text-shadow-md">진행사항2</h2>
                     <div className="bg-[#3eabfe] w-full flex-1 flex items-center justify-center">
-                        <div className={`flex w-full h-full items-center justify-center ${status2Img[weather.skyStatus]} bg-[length:50%] bg-center bg-no-repeat `}>
-                            <span className="text-gray-800 text-5xl">
-                                {/* {weather.length ? weather[4]?.value + weather[4]?.unit : "조회불가"} */}
-                                {weather.temperature}°C
-                            </span>
+                    <div className="w-full p-4 bg-[#1e4da7] rounded-b-lg text-xl font-gothic">
+                        <div className="flex justify-between items-center border-b border-black py-2">
+                            <span>사전작업2</span>
                         </div>
+                        <div className="flex justify-between items-center border-b border-black py-2">
+                            <span>서비스 차단2</span>
+                        </div>
+                        <div className="flex justify-between items-center border-b border-black py-2">
+                            <span>데이터복제2</span>
+                        </div>
+                        <div className="flex justify-between items-center py-2">
+                            <span>서버 시작2</span>
+                        </div>
+                    </div>
                         {/* <style jsx>{`
                             .bg-image {
                                 background-image: url("/차량수량.png");
@@ -61,19 +57,6 @@ const TrafficWeather = ({ trafficInfo, weather }) => {
                                 background-position: center;
                             }
                         `}</style> */}
-                    </div>
-                    <div className="flex justify-between items-center bg-white w-full flex-2 px-2 py-2 rounded-b-lg">
-                        <span className="text-gray-800 text-3xl font-gothic-bold flex-1 text-center">
-                            {weather.skyStatus}
-                        </span>
-                        <div className="text-gray-600 text-sm flex-1">
-                            <div>
-                                강수확률 <span className="text-blue-600">{weather.precipitationProbability}%</span>
-                            </div>
-                            <div>
-                                예상강수량 <span className="text-blue-600">{weather.precipitationAmount}</span>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
