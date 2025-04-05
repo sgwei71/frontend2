3bf0044d87903f636df0d29151aa1e7e7757b78f
sunggyu <sunggyu@ibk.co.kr>
2025-04-06 03:07:43 +0900

    변경7

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -89,7 +89,7 @@ const Header = () => {
                 </FormControl>
             </div>
             {/* 가운데 */}
-            <div className="flex justify-center items-center">
+            <div className="w-[37%] flex justify-center items-center">
                 <div className="bg-white/20 px-6 py-2 rounded-lg shadow-md">
                     <span className="text-2xl sm:text-3xl font-bold tracking-tight text-white">
                         계정성 업무 전환
