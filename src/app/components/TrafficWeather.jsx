56897935c760f89ade971b7a0f32bd1bf1886286
sunggyu <sunggyu@ibk.co.kr>
2025-04-06 01:58:01 +0900

    변경7

--- src/app/components/TrafficWeather.jsx
+++ src/app/components/TrafficWeather.jsx
@@ -12,7 +12,26 @@ const status2Img = {
 const TrafficWeather = ({ trafficInfo, weather }) => {
     return (
         <div className="w-[37%] h-full flex items-center justify-center text-white">
-
+            <div className="flex justify-center items-center w-full h-full space-x-6 px-16 py-8">
+                {/* 교통상황 */}
+                <div className="flex flex-col justify-between bg-blue-common text-white font-gothic-bold shadow-lg rounded-lg w-[45%] h-full">
+                    <h2 className="text-center text-2xl text-shadow-md p-4">   </h2>
+                    <div className="w-full p-4 bg-[#1e4da7] rounded-b-lg text-xl font-gothic">
+                        <div className="flex justify-between items-center border-b border-black py-2">
+                            <span>  </span>
+                        </div>
+                        <div className="flex justify-between items-center border-b border-black py-2">
+                            <span>   </span>
+                        </div>
+                        <div className="flex justify-between items-center border-b border-black py-2">
+                            <span>   </span>
+                        </div>
+                        <div className="flex justify-between items-center py-2">
+                            <span>     </span>
+                        </div>
+                    </div>
+                </div>
+           </div>
         </div>
     );
 };
